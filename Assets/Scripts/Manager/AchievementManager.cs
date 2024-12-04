a75362690c878955b0c696465f5953aec1f5e3c9
leesiwoo <s24028@gsm.hs.kr>
2024-12-04 18:29:24 +0900

    Faze 2 Pattern 7 Added, Achievement System Making

---
+++ Assets/Scripts/Manager/AchievementManager.cs
@@ -0,0 +1,63 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementState
+{
+    public string key;
+    public int isClear;
+    public AchievementState(string key, int isClear)
+    {
+        this.key = key;
+        this.isClear = isClear;
+    }
+}
+
+public class AchievementManager : MonoBehaviour
+{
+    public static AchievementManager instance;
+    public AchievementState[] achievementStates;
+
+    public AchievementSO[] achievementSOs;
+
+    private void Awake()
+    {
+        if (instance == null) { instance = this; DontDestroyOnLoad(gameObject); }
+        else Destroy(instance);
+    }
+    public void InitAchi()
+    {
+        if (PlayerPrefs.GetInt("InitState", 0) == 1) return;
+        //PlayerPrefs 내부 값 초기화
+        for(int i = 0; i < achievementStates.Length; i++)
+        {
+            achievementStates[i] = new AchievementState(achievementSOs[i].key, 
+                PlayerPrefs.HasKey(achievementSOs[i].key) ? PlayerPrefs.GetInt(achievementSOs[i].key) : 0);
+            PlayerPrefs.SetInt(achievementStates[i].key, achievementStates[i].isClear);
+        }
+        PlayerPrefs.SetInt("InitState", 1);
+        PlayerPrefs.Save();
+    }
+    public void SaveAchi(string key, int clear)
+    {
+        if(PlayerPrefs.HasKey(key))
+        {
+            PlayerPrefs.SetInt(key, clear);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.Log($"Key {key} is not inited!");
+        }
+    }
+    public AchievementState FindAchiWithKey(string key)
+    {
+        foreach(AchievementState state in achievementStates)
+        {
+            if (state.key == key) return state;
+        }
+        return null;
+    }
+}
--- Assets/Scripts/UIAppear.cs
+++ Assets/Scripts/UIAppear.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,26 +8,52 @@ public class UIAppear : MonoBehaviour
     public Text Title;
     public Text Description;
     public Image icon;
+    private string key;
 
-    private float Duration = 1f;
+    private float Duration = 3f;
+    private float EndDuration = 1.2f;
     private float TimeElapsed = 0f;
+
+    private RectTransform RT;
+    private Vector3 EndRT;
+    private Vector3 StartRT;
     void Start()
     {
         Title.text = ASO.Title;
         Description.text = ASO.Description;
         icon.sprite = ASO.Icon;
-    }
+        RT = GetComponent<RectTransform>();
+        key = ASO.key;
 
-    // Update is called once per frame
-    void Update()
+        StartRT = RT.anchoredPosition;
+        EndRT = new Vector3(RT.anchoredPosition.x, 33.405f, 0f);
+        StartCoroutine(Appear());
+    }
+    IEnumerator Appear()
     {
-        while(TimeElapsed < Duration)
+        while (TimeElapsed < Duration)
         {
             TimeElapsed += Time.deltaTime;
             float t = TimeElapsed / Duration;
             t = Linear(t);
+            RT.anchoredPosition = Vector3.Lerp(RT.anchoredPosition, EndRT, t);
 
+            yield return null;
         }
+        yield return new WaitForSeconds(3f);
+        TimeElapsed = 0f;
+        while (TimeElapsed < EndDuration)
+        {
+            TimeElapsed += Time.deltaTime;
+            float t = TimeElapsed / EndDuration;
+            t = Linear(t);
+            RT.anchoredPosition = Vector3.Lerp(EndRT, StartRT, t);
+
+            yield return null;
+        }
+        AchievementManager.instance.SaveAchi(key, 1);
+        Debug.Log($"Achi State Saved! Name : {key}");
+        Destroy(gameObject);
     }
     float Linear(float x)
     {
